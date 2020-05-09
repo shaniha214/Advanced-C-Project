9c10fea76343cf0d17dd772c8df7c301b6501b31
Guy Arieli <guyarieli1997@gmail.com>
2020-05-09 18:02:17 +0300

    second commit

---
+++ Headers/linkedList.h
@@ -0,0 +1,26 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include "boards.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
+
+typedef struct _moveCell {
+  Move move;
+  struct _moveCell *next, *prev;
+} moveCell;
+
+typedef struct _movesList {
+  moveCell * head;
+  moveCell * tail;
+} movesList;
+
+void makeEmptyList(movesList *lst);
+moveCell * createCell(char row, char col);
+void printList(movesList *lst);
+void freeList(movesList *lst);
+
+#endif
